14d7b7e126ae453476d834cf43a84cb36f9c6c95
Kepler <12010823@mail.sustech.edu.cn>
2022-09-27 01:55:36 +0800

    Generated .exe file
    Signed-off-by: Kepler <12010823@mail.sustech.edu.cn>

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from datetime import date, timedelta
-import datetime
+import sys
+sys.path.append(r"C:\Users\Administrator\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages")
 from ics import Calendar,Event
 from openpyxl import load_workbook
 from chinese_calendar import is_workday
+import tatsu
 
 
 def AddCanlendar(Cal,course):
